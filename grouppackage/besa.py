f9abc4ca8335613c88f7e2ca335c2665ee503728
besa-vinceadrian <vinceadrianbesa@gmail.com>
2025-06-04 15:28:19 +0800

    add besa module

--- grouppackage/besa.py
+++ grouppackage/besa.py
@@ -19,37 +19,29 @@ def besa_menu():
                 print("Age: 20")
                 print("Birthday: June 4, 2005")
                 print("Address: Central Bicutan, Taguig City\n")
-
             case 2:
                 print("\nGoals:")
                 print("1. Become a software engineer")
                 print("2. Make a positive impact in the tech industry")
                 print("3. Continuously learn and grow in my field")
                 print("4. Contribute to open-source projects\n")
-
             case 3:
                 print("\nMotto:")
                 print("'Everyday you wake up, be better.'\n") 
-
             case 4: 
                 print("\nComment from Bualat:")
                 # TODO: Add comment from Bualat
-
             case 5:
                 print("\nComment from Maestre:")
                 # TODO: Add comment from Maestre
-
             case 6:
                 print("\nComment from Salespara:")
                 # TODO: Add comment from Salespara
-
             case 7:
                 print("\nComment from Serquina:")
                 # TODO: Add comment from Serquina
-
             case 8: 
                 print("Program Terminated.")
                 break
-
             case _:
                 print("Invalid choice. Please select a valid option (1-8).")
